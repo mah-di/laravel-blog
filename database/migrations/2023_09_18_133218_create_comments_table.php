d4288f5326afd34fb82352f810d86a49d4590a1a
Iqbal Mahdi <mahdiiqbal37@gmail.com>
2023-09-18 19:46:37 +0600

    Added migration file to create comments table

---
+++ database/migrations/2023_09_18_133218_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('body');
+            $table->foreignId('user_id')->nullable()->constrained('users', 'id')->nullOnDelete();
+            $table->foreignId('blog_id')->constrained('blogs', 'id')->cascadeOnDelete();
+            $table->foreignId('parent_id')->nullable()->constrained('comments', 'id')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
