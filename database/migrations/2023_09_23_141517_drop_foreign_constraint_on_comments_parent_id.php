31afb2bc823e9cae0fe07fcd80f669c0ac5f6fa2
Iqbal Mahdi <mahdiiqbal37@gmail.com>
2023-09-23 23:54:38 +0600

    Dropped constraints on "parent_id" and "blog_id" on "comments" table
    Because of the polymorphic relations between "likes" & "blogs", and "likes" & "comments", cascadeOnDelete wasn't deleting the related rows on "likes" table, as there can be no constraints in polymorphic relations. To troubleshoot this, manual deletion needs to be implemented. This is the first step of the process to make comments independent so they can be manually deleted.

---
+++ database/migrations/2023_09_23_141517_drop_foreign_constraint_on_comments_parent_id.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign(['parent_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->foreignId('parent_id')->constrained('comments', 'id')->cascadeOnDelete();
+        });
+    }
+};
